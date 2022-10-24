21240225cf40955bc7323ba24bcd5734d0dc89a7
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2022-10-24 23:56:50 +0200

    added class AtmosphericPressureAtSeaLevel

---
+++ src/pl/ciosmak/sections/sectionOne/AtmosphericPressureAtSeaLevel.java
@@ -0,0 +1,25 @@
+package pl.ciosmak.sections.sectionOne;
+
+import pl.ciosmak.sections.Section;
+
+public class AtmosphericPressureAtSeaLevel extends Section
+{
+    public AtmosphericPressureAtSeaLevel(final String parameters)
+    {
+        int tmpAtmosphericPressureAtSeaLevel = Integer.parseInt(parameters.substring(1));
+        if (tmpAtmosphericPressureAtSeaLevel < 1000)
+        {
+            tmpAtmosphericPressureAtSeaLevel += 10000;
+        }
+        tmpAtmosphericPressureAtSeaLevel /= 10;
+        atmosphericPressureAtSeaLevel = Integer.toString(tmpAtmosphericPressureAtSeaLevel);
+    }
+
+    @Override
+    public void show()
+    {
+        System.out.println("Ciśnienie atmosferyczne na poziomie stacji: " + atmosphericPressureAtSeaLevel);
+    }
+
+    private final String atmosphericPressureAtSeaLevel;
+}
