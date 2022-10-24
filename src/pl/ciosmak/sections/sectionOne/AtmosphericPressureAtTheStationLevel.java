f3f0a1aa4a2692eadf704fddcf28204bae556a8c
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2022-10-24 23:07:22 +0200

    added class AtmosphericPressureAtTheStationLevel

---
+++ src/pl/ciosmak/sections/sectionOne/AtmosphericPressureAtTheStationLevel.java
@@ -0,0 +1,32 @@
+package pl.ciosmak.sections.sectionOne;
+
+import pl.ciosmak.sections.Section;
+
+public class AtmosphericPressureAtTheStationLevel extends Section
+{
+    public AtmosphericPressureAtTheStationLevel(final String parameters)
+    {
+        double tmpAtmosphericPressureAtTheStationLevel = Double.parseDouble(parameters.substring(1));
+        if (tmpAtmosphericPressureAtTheStationLevel < 1000)
+        {
+            tmpAtmosphericPressureAtTheStationLevel += 1000;
+        }
+        tmpAtmosphericPressureAtTheStationLevel /= 10;
+        atmosphericPressureAtTheStationLevel = replacePeriodWithComma(new StringBuilder(Double.toString(tmpAtmosphericPressureAtTheStationLevel)));
+    }
+
+    @Override
+    public void show()
+    {
+        System.out.println("Ciśnienie atmosferyczne na poziomie stacji: " + atmosphericPressureAtTheStationLevel);
+    }
+
+    private String replacePeriodWithComma(StringBuilder text)
+    {
+        int indexOfPeriod = text.indexOf(".");
+        text.replace(indexOfPeriod, indexOfPeriod + 1, ",");
+        return text.toString();
+    }
+
+    private final String atmosphericPressureAtTheStationLevel;
+}
