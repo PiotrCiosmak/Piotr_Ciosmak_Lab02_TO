75bf9383f5ba8afc2840901664cae37e562e77a1
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2022-10-24 16:47:28 +0200

    WindIndex implemented in Main

--- src/pl/ciosmak/Main.java
+++ src/pl/ciosmak/Main.java
@@ -2,6 +2,11 @@ package pl.ciosmak;
 
 import pl.ciosmak.data.RemoteDataProvider;
 import pl.ciosmak.extractor.Extractor;
+import pl.ciosmak.sections.Section;
+import pl.ciosmak.sections.sectionZero.Date;
+import pl.ciosmak.sections.sectionZero.StationID;
+import pl.ciosmak.sections.sectionZero.StationTypeID;
+import pl.ciosmak.sections.sectionZero.WindIndex;
 
 import java.util.ArrayList;
 
@@ -14,17 +19,16 @@ public class Main
         RemoteDataProvider remoteDataProvider = new RemoteDataProvider();
         remoteDataProvider.initialize();
         ArrayList<String> linesOfData = splitData(remoteDataProvider.getData());
-        int licznik = 0;
+        ArrayList<Section> listOfSection = new ArrayList<>();
         for (var line : linesOfData)
         {
             Extractor extractor = new Extractor(line);
-            licznik++;
-            System.out.println(licznik);
             if (!extractor.getStationTypeID().equals("AAXX"))
-            {
                 continue;
-            }
-
+            listOfSection.add(new StationID(extractor.getStationID()));
+            listOfSection.add(new Date(extractor.getYear(), extractor.getMonth(), extractor.getDay(), extractor.getHour(), extractor.getMinute()));
+            listOfSection.add(new StationTypeID(extractor.getStationTypeID()));
+            listOfSection.add(new WindIndex(extractor.getTimeOfObservationAndWindIndex()));
         }
     }
 }
