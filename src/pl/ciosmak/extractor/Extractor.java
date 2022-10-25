e3fc53fd077ec34f687a020b130bcc6f580966e1
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2022-10-25 15:27:42 +0200

    TODO done

--- src/pl/ciosmak/extractor/Extractor.java
+++ src/pl/ciosmak/extractor/Extractor.java
@@ -49,7 +49,7 @@ public class Extractor
             {
                 rainfall = tmpData;
             }
-            else if (tmpData.charAt(0) == '7')//TODO Uważać na to ze albo są dwie liczby i // albo // i dwie liczby
+            else if (tmpData.charAt(0) == '7')
             {
                 presentAndPastWeatherCondition = tmpData;
             }
